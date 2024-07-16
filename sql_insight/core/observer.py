a812b76ade5a9ee0872244d1f2693724a00dbba9
nickBes <58567842+nickBes@users.noreply.github.com>
2024-07-16 21:34:27 +0300

    feat: replaced register with on exit and on enter events

--- sql_insight/core/observer.py
+++ sql_insight/core/observer.py
@@ -7,20 +7,34 @@ ObserverMethod: TypeAlias = Callable[[SelfExpressionObserver, E], None]
 
 
 class ExpressionObserver:
-    observer_method_names: dict[str, str] = dict()
+    on_exit_method_names: dict[str, str] = dict()
+    on_enter_method_names: dict[str, str] = dict()
 
     @classmethod
-    def register(cls, expression_kind: type[Expression]):
-        def register_observer_method(
+    def on_exit(cls, expression_kind: type[Expression]):
+        def register_on_exit_method(
             observer_method: ObserverMethod[SelfExpressionObserver, E],
         ) -> ObserverMethod[SelfExpressionObserver, E]:
-            cls.observer_method_names[expression_kind.__name__] = (
+            cls.on_exit_method_names[expression_kind.__name__] = (
                 observer_method.__name__
             )
 
             return observer_method
 
-        return register_observer_method
+        return register_on_exit_method
+
+    @classmethod
+    def on_enter(cls, expression_kind: type[Expression]):
+        def register_on_enter_method(
+            observer_method: ObserverMethod[SelfExpressionObserver, E],
+        ) -> ObserverMethod[SelfExpressionObserver, E]:
+            cls.on_enter_method_names[expression_kind.__name__] = (
+                observer_method.__name__
+            )
+
+            return observer_method
+
+        return register_on_enter_method
 
 
 class ExpressionWalker:
@@ -28,15 +42,33 @@ class ExpressionWalker:
         self.expression = expression
         self.observers = observers
 
+    def __walk_recursive(self, expression: Expression):
+        on_enter_methods = []
+        on_exit_methods = []
+
+        for observer in self.observers:
+            on_enter_method_name = observer.on_enter_method_names.get(
+                expression.__class__.__name__
+            )
+
+            on_exit_method_name = observer.on_exit_method_names.get(
+                expression.__class__.__name__
+            )
+
+            if on_enter_method_name:
+                on_enter_methods.append(observer.__getattribute__(on_enter_method_name))
+
+            if on_exit_method_name:
+                on_exit_methods.append(observer.__getattribute__(on_exit_method_name))
+
+        for on_enter_method in on_enter_methods:
+            on_enter_method(expression)
+
+        for child in expression.iter_expressions():
+            self.__walk_recursive(child)
+
+        for on_exit_method in on_exit_methods:
+            on_exit_method(expression)
+
     def walk(self):
-        for expression in self.expression.bfs():
-            for observer in self.observers:
-                observer_method_name = observer.observer_method_names.get(
-                    expression.__class__.__name__
-                )
-
-                if observer_method_name:
-                    observer_method = observer.__getattribute__(observer_method_name)
-
-                    if observer_method:
-                        observer_method(expression)
+        self.__walk_recursive(self.expression)
--- sql_insight/observers/PartitionObserver.py
+++ sql_insight/observers/PartitionObserver.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 from sql_insight.core.observer import ExpressionObserver
 from sql_insight.observers.utils import get_column_expression_tables
-from sql_insight.core.utils import get_expressions_by_recursive_path
-from sqlglot.expressions import Where, Column, Condition, Join, Table
+from sql_insight.core.utils import get_expressions_by_recursive_path, get_parent
+from sqlglot.expressions import Where, Column, Condition, Join, Select
+from sqlglot.lineage import lineage
 
 
 @dataclass
@@ -14,7 +15,7 @@ class PartitionCandidate:
 class PartitionObserver(ExpressionObserver):
     partition_candidates: list[PartitionCandidate] = []
 
-    @ExpressionObserver.register(Where)
+    @ExpressionObserver.on_exit(Where)
     def get_where_columns(self, where_expression: Where):
         for column in get_expressions_by_recursive_path(
             where_expression, (Condition, Column)
@@ -26,7 +27,7 @@ class PartitionObserver(ExpressionObserver):
                 )
             )
 
-    @ExpressionObserver.register(Join)
+    @ExpressionObserver.on_exit(Join)
     def get_join_columns(self, join_expression: Join):
         for column in get_expressions_by_recursive_path(
             join_expression, (Condition, Column)
